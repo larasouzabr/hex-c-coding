9f87016198bdb05a7df0cc7af24cf8b85d9e299c
Lara Souza <80922589+larasouzabr@users.noreply.github.com>
2021-05-18 14:17:33 -0300

    Create 2-printf.c

---
+++ 0x00-hello_world/2-printf.c
@@ -0,0 +1,10 @@
+/**
+* main - entry points
+* Return: always 0, sucess
+*/
+#include <stdio.h>
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
