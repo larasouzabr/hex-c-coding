94701cf0d38dfe77f473f16f193e91778537fee0
Lara Souza <80922589+larasouzabr@users.noreply.github.com>
2021-05-18 14:13:16 -0300

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main-prints the lowercase alphabet in reverse
+*Return: it returns 0
+*/
+int main(void)
+{
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
+putchar('\n');
+return (0);
