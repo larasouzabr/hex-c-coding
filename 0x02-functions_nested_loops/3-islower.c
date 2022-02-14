5fc68c0aaddc5459e4e9091801ca1b012bca73df
Lara <laragabriellysouzabatista@gmail.com>
2022-02-14 13:20:36 +0000

    tasks

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks for lowercase letter
+ * @c: lowercase letters
+ * Return: 1 if c, 0 for everything else
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+return (0);
+}
+
